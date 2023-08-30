011a45d572de3c6dce0e355a4962fd2eecbf4c80
ashikuzzaman-abir <ashikuzzamanabir@outlook.com>
2023-08-30 11:55:31 +0600

    cors added

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './db.js';
 import productsRoutes from './routes/productsRoutes.js';
+import cors from 'cors';
 
 const app = express();
 app.use(express.json());
+app.use(cors())
 dotenv.config();
 connectDB();
 
