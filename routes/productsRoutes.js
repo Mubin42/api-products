caece672d69bdea5adf677e7ffdbfe2c7d9a044c
Kazi Ehsanul Mubin <kazimubin.46@gmail.com>
2023-11-01 15:33:14 +0600

    mongo fix

--- routes/productsRoutes.js
+++ routes/productsRoutes.js
@@ -8,6 +8,7 @@ import deleteProduct from '../controllers/products/deleteProduct.js';
 
 const router = express.Router();
 router.use('/products/:id', checkProduct);
+
 router.get('/products', getAllProduct);
 router.post('/products', createProduct);
 router.get('/products/:id', getSingleProduct);
